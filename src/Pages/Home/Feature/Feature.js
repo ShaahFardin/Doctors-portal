439e058aeb767cea40839b057f26f64d1a26c745
shaahfardin <fardinshaah@gmail.com>
2022-11-12 02:28:31 +0600

    3 card addedd

--- src/Pages/Home/Feature/Feature.js
+++ src/Pages/Home/Feature/Feature.js
@@ -1,14 +1,30 @@
 import React from 'react';
+import clockIcon from '../../../assets/icons/clock.svg'
+import markerIcon from '../../../assets/icons/marker.svg'
+import phoneIcon from '../../../assets/icons/phone.svg'
 
 const Feature = () => {
     return (
-        <div className="card w-96 bg-neutral text-neutral-content">
-            <div className="card-body items-center text-center">
-                <h2 className="card-title">Cookies!</h2>
-                <p>We are using cookies for no reason.</p>
-                <div className="card-actions justify-end">
-                    <button className="btn btn-primary">Accept</button>
-                    <button className="btn btn-ghost">Deny</button>
+        <div className='flex justify-evenly mt-36'>
+            <div className="rounded-xl w-[400px] h-[170px] bg-primary flex justify-evenly items-center">
+                <img className='h-20 w-20' src={clockIcon} alt='' />
+                <div className='text-white'>
+                    <h1 className='text-xl font-bold mb-2'>Opening Hours</h1>
+                    <p>Lorem impusm king eide frage deutchland </p>
+                </div>
+            </div>
+            <div className="rounded-xl w-[400px] h-[170px] bg-accent flex justify-evenly items-center">
+                <img className='h-20 w-20' src={markerIcon} alt='' />
+                <div className='text-white'>
+                    <h1 className='text-xl font-bold mb-2'>Opening Hours</h1>
+                    <p>Lorem impusm king eide frage deutchland </p>
+                </div>
+            </div>
+            <div className="rounded-xl w-[400px] h-[170px] bg-primary flex justify-evenly items-center">
+                <img className='h-20 w-20' src={phoneIcon} alt='' />
+                <div className='text-white'>
+                    <h1 className='text-xl font-bold mb-2'>Opening Hours</h1>
+                    <p>Lorem impusm king eide frage deutchland </p>
                 </div>
             </div>
         </div>
